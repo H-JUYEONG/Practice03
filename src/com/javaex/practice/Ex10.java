0bd930d82cc2967d4d0ec2464e26123ca2c2129d
H-JUYEONG <dud9902@gmail.com>
2024-06-27 17:38:47 +0900

    수정

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -8,27 +8,23 @@ public class Ex10 {
 
 		// 정수 다섯개를 입력받아 가장 큰 수를 출력하세요.
 
-		//for문 사용
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("숫자를 입력하세요");
 
-		int[] num = new int[5]; // 정수 다섯개를 입력받으므로 배열 크기는 5
-
-		for (int i = 0; i < 5; i++) { // 0, 1, ,2, 3, 4 --> 5번 반복
+		int max = 0;
+		
+		for (int i = 0; i < 5; i++) {
 			System.out.print("숫자: ");
-			num[i] = sc.nextInt();
-		}
+			int num = sc.nextInt();
 
-		int max = num[0]; // 첫번째로 입력받은 값을 최대값으로 설정
-
-		// num[0]이 max로 설정되어 있으므로 i값을 1로 세팅 = num[1]부터 비교
-		for (int i = 1; i < num.length; i++) {
-			if (max < num[i]) { // num[0]과 num[1] 값을 비교
-				max = num[i]; // num[1]이 num[0]보다 크면 max에 num[1] 값으로 갱신
+			if (num > max) {
+				max = num;
 			}
-		}
 
-		System.out.println("최대값은 " + max + "입니다.");
+		}
+		
+		System.out.println("최대값은 " + max + " 입니다.");
 
 		sc.close();
 
@@ -65,7 +61,7 @@ public class Ex10 {
 
 		sc.close();
 		*/
-
+		
 	}
 
 }
