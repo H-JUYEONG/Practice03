527e55a7572cc5f81475edcdf5ba0de162ad69d2
H-JUYEONG <dud9902@gmail.com>
2024-06-27 21:38:06 +0900

    수정

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -6,7 +6,7 @@ public class Ex09 {
 
 		// 다음과 같이 출력하세요
 
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			for (int j = 1; j <= 10; j++) {
 				System.out.print(j + i + "\t");
 
